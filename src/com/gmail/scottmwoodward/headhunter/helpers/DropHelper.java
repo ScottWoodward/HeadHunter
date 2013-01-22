99877d6dc29e1b208d6c54214299abc208278b14
Fogest <fogestjv@gmail.com>
2013-01-22 18:36:36 -0500

    Updating to work with Bukkit 1.4.7

--- src/com/gmail/scottmwoodward/headhunter/helpers/DropHelper.java
+++ src/com/gmail/scottmwoodward/headhunter/helpers/DropHelper.java
@@ -10,7 +10,7 @@ package com.gmail.scottmwoodward.headhunter.helpers;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_4_6.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
